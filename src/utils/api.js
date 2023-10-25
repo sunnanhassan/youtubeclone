aa4037166edd46bfa8dafd243b71230932223eb5
Sunnan <sannanhassan10@gmail.com>
2023-10-25 20:47:11 +0500

    add

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const axios = require("axios");
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: {
+    hl: "en",
+    gl: "US",
+  },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY,
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
